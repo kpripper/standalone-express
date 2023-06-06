70a3cf91ea6bd88576c1b3142c94c9f7a46aa72f
kpripper <102256496+kpripper@users.noreply.github.com>
2023-06-06 23:07:42 +0300

    dev-josh.hashnode.dev

--- api/app.js
+++ api/app.js
@@ -1,20 +1,17 @@
-const app = require('express')()
-const { v4 } = require('uuid')
+// [index.js] with server app export.
 
-app.listen(3000, () => {
-  console.log('Сервер 1 запущено на порту 3000')
-})
+const express = require("express");
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
-})
+const port = process.env.PORT || 4000;
+const app = express();
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params
-  res.end(`Item: ${slug}`)
-})
+app.get('/hello-world', (request, response) => {
+  response.send('Hello World!');
+});
 
-module.exports = app
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+// Export the Express API
+module.exports = app;
